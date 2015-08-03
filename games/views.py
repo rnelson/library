1c769678184d6782e8d64b33e608ba09b81c5709
Ross Nelson <ross.nelson@gmail.com>
2015-08-03 12:36:54 -0500

    Display added date on game information

--- games/views.py
+++ games/views.py
@@ -2,10 +2,13 @@ from __future__ import unicode_literals
 from django.shortcuts import get_object_or_404, render_to_response
 from games.models import Game, Publisher
 from django.template.context import RequestContext
+from django.utils import formats
+from datetime import datetime
+
 
 # Create your views here.
 def index(request):
-    games = Game.objects.all().order_by('-updated_at')
+    games = Game.objects.all().order_by('name')
     context = RequestContext(request)
     return render_to_response('games/index_games.html', {'games': games}, context)
 
@@ -23,8 +26,9 @@ def publishers(request):
 
 def game_details(request, slug):
     game = get_object_or_404(Game, slug=slug)
+    added_date = formats.date_format(game.created_at, 'SHORT_DATE_FORMAT')
     context = RequestContext(request)
-    return render_to_response('games/details.html', {'game': game}, context)
+    return render_to_response('games/details.html', {'game': game, 'added_date': added_date}, context)
 
 
 def publisher_details(request, slug):
