c4605d2149b3fe96d893c186f6a77a53be06364f
Ross Nelson <ross.nelson@gmail.com>
2015-08-02 20:00:58 -0400

    Remove unused import

--- games/models.py
+++ games/models.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
-from libxml2mod import properties
 
 
 @python_2_unicode_compatible
